4b2b2e702329b513233e8da391202436434b2b7b
penand <marzone.ap@gmail.com>
2022-01-31 18:18:49 +0100

    feat: add cors to project

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const port = process.env.PORT || 3000;
 
@@ -12,7 +13,15 @@ db.on('error', (error) => console.error(error));
 db.once('open', () => console.log('Connected to Database'));
 
 app.use(express.json());
+app.use(cors());
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'DELETE, PUT');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+ });
+ 
 const quotesRouter = require('./routes/quotes');
 app.use('/quotes', quotesRouter);
 
